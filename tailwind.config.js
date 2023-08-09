e109d4f4e51502732c71a9ad260ca52da9427fca
MidzDev <wiwm88@gmail.com>
2023-08-09 21:35:49 +0800

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        secondary: "var(--secondary)",
-      },
-    },
-  },
-  plugins: [],
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				secondary: 'var(--secondary)'
+			}
+		}
+	},
+	plugins: []
 };
